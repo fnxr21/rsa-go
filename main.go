cc4dcd9bdeb113a32a24f5fba1e93227ce888192
fandi.nxr <fandinur214@gmail.com>
2024-01-10 10:54:30 +0700

    update

--- main.go
+++ main.go
@@ -1,11 +1,8 @@
 package main
 
 import (
-	// "crypto"
-	"crypto"
 	"crypto/rand"
 	"crypto/rsa"
-	"crypto/sha256"
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
@@ -13,111 +10,67 @@ import (
 )
 
 func main() {
-privateKey :=GeneratePrivateKey()
-// CreateFiless()
-FullRsa(privateKey)
-}
+	// // Generate RSA private key
+	// privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	// if err != nil {
+	// 	fmt.Println("Error generating private key:", err)
+	// 	return
+	// }
 
+	// // Save private key to file
+	// privateKeyPEM := pem.EncodeToMemory(&pem.Block{
+	// 	Type:  "RSA PRIVATE KEY",
+	// 	Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	// })
+	// err = ioutil.WriteFile("private.pem", privateKeyPEM, 0600)
+	// if err != nil {
+	// 	fmt.Println("Error saving private key to file:", err)
+	// 	return
+	// }
 
-func GeneratePrivateKey() *rsa.PrivateKey {
-// Generate the private key once and store it securely
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	// fmt.Println("Private key saved to private.pem")
+
+	// Load private key from file
+	privateKeyFile, err := os.ReadFile("gate-sap-private.pem")
 	if err != nil {
-	panic(err)
+		fmt.Println("Error reading private key from file:", err)
+		return
+	}
+	block, _ := pem.Decode(privateKeyFile)
+	if block == nil || block.Type != "RSA PRIVATE KEY" {
+		fmt.Println("Error decoding private key")
+		return
+	}
+	privateKeyFromPEM, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		fmt.Println("Error parsing private key:", err)
+		return
 	}
 
-   // Store the private key securely (e.g., in a password-protected file)
-   privateKeyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
-   pem.Encode(os.Stdout, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: privateKeyBytes})
+	// Generate RSA public key
+	publicKey := &privateKeyFromPEM.PublicKey
+	publicKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: x509.MarshalPKCS1PublicKey(publicKey),
+	})
 
-   return privateKey
+	fmt.Println("Public key:")
+	fmt.Println(string(publicKeyPEM))
 
-}
-
-
-func FullRsa(ok *rsa.PrivateKey) {
-// Generate the private key once and store it securely
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
-    
+	// Encrypt a message with the public key
+	message := "Hello, RSA!"
+	ciphertext, err := rsa.EncryptPKCS1v15(rand.Reader, publicKey, []byte(message))
 	if err != nil {
-	panic(err)
+		fmt.Println("Error encrypting message:", err)
+		return
 	}
 
-   // Store the private key securely (e.g., in a password-protected file)
-   privateKeyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
-   pem.Encode(os.Stdout, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: privateKeyBytes})
+	// Decrypt the message with the private key
+	decryptedMessage, err := rsa.DecryptPKCS1v15(rand.Reader, privateKeyFromPEM, ciphertext)
+	if err != nil {
+		fmt.Println("Error decrypting message:", err)
+		return
+	}
 
-   // Generate a new public key each time
-   publicKey := &privateKey.PublicKey
-   publicKeyBytes, err := x509.MarshalPKIXPublicKey(publicKey)
-   if err != nil {
-       panic(err)
-   }
-   pem.Encode(os.Stdout, &pem.Block{Type: "PUBLIC KEY", Bytes: publicKeyBytes})
-
-   // Encrypt a message using the public key
-   message := []byte("This is a secret message")
-   encryptedMessage, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, publicKey, message, nil)
-   if err != nil {
-       panic(err)
-   }
-   fmt.Println("Encrypted message:", encryptedMessage)
-
-   // Decrypt the message using the private key
-   decryptedMessage, err := privateKey.Decrypt(nil, encryptedMessage, &rsa.OAEPOptions{Hash: crypto.SHA256})
-   if err != nil {
-       panic(err)
-   }
-   fmt.Println("Decrypted message:", string(decryptedMessage))
-}
-
-
-func CreateFiless()  {
-    pemBytes, err := os.ReadFile("gate-sap-private.pem")
-if err != nil {
-    fmt.Println("Error reading PEM file:", err)
-    return
- 
-}
-block, _ := pem.Decode(pemBytes)
-if block == nil {
-    fmt.Println("Failed to decode PEM block")
-    return
-}
-
-fmt.Println(block)
-
-pubKey, err := x509.ParsePKIXPublicKey(block.Bytes)
-if err != nil {
-    fmt.Println("Failed to parse RSA public key:", err)
-    return
-}
-rsaPubKey := pubKey.(*rsa.PublicKey) // Type assertion to get rsa.PublicKey
-
-fmt.Println(rsaPubKey)
-}
-
-
-func Createfile()  {
-    pubDER := x509.MarshalPKCS1PublicKey(&privateKey.PublicKey)
-pubBlock := pem.Block{
-    Type:  "PUBLIC KEY",
-    Bytes: pubDER,
-}
-
-    err:= error
-    
-    pemFile, err = os.Create("public.pem") // Adjust filename as needed
-if err != nil {
-    fmt.Println("Error creating PEM file:", err)
-    return
-}
-defer pemFile.Close()
-
-err = pem.Encode(pemFile, &pubBlock)
-if err != nil {
-    fmt.Println("Error encoding PEM block:", err)
-    return
-}
-fmt.Println("Public key saved to public.pem")
+	fmt.Println("Original message:", string(decryptedMessage))
 }
